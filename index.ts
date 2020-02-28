335c06c349858cabfbac01624b103a5ec9a787a3
Gustavo Santos <gustavofsantos@outlook.com>
2020-02-27 21:13:41 -0300

    Added first validation file

---
+++ index.ts
@@ -0,0 +1,25 @@
+export class ValidatorSuccess { }
+export class ValidatorFailure {
+  constructor(public reason: string) { }
+}
+
+export type EitherValidator = ValidatorSuccess | ValidatorFailure;
+
+export function combineValidators<A>(...validators: ((input: A) => Promise<EitherValidator>)[]): (input: A) => Promise<EitherValidator> {
+  return async function validator(input: A) {
+    let result = new ValidatorSuccess();
+
+    try {
+      for (const v of validators) {
+        result = await v(input);
+        if (result instanceof ValidatorFailure) {
+          return result;
+        }
+      }
+
+      return result;
+    } catch (err) {
+      return new ValidatorFailure(err);
+    }
+  }
+}
